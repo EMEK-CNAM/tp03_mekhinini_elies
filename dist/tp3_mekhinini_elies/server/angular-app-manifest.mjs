768acfa3755f5173560b7b74c5bb33d458986944
mekhinini <elies.mekhinini@etu.unistra.fr>
2025-10-24 22:25:03 +0200

    push désespéré

---
+++ dist/tp3_mekhinini_elies/server/angular-app-manifest.mjs
@@ -0,0 +1,32 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: '/',
+  locale: undefined,
+  routes: [
+  {
+    "renderMode": 1,
+    "route": "/"
+  },
+  {
+    "renderMode": 1,
+    "route": "/new"
+  },
+  {
+    "renderMode": 1,
+    "route": "/pollution/*"
+  },
+  {
+    "renderMode": 1,
+    "redirectTo": "/",
+    "route": "/**"
+  }
+],
+  entryPointToBrowserMapping: undefined,
+  assets: {
+    'index.csr.html': {size: 504, hash: '3e0ff24972fd330cff26978bea91e88e61b45c101332b9e15ee2f499554f66fb', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1017, hash: 'ba3b739a238d76f031f935e02d4141edddd9667cc2dc6c550094f5b7b85528b5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
+  },
+};
